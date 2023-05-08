fc44215610fd95a04a69127be0713f3819fe9864
towel17 <towel1703@inbox.ru>
2023-05-08 20:47:44 +0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="./assets/style.css">
+        <title>Главная</title>
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <a href="#"><img src="./assets/imeges/logo.png" alt="#" class="logo"></a>
+                <form method="get">
+                    <input type="text" placeholder="поиск" name="">
+                    <input type="submit" value="найти">
+                </form>
+                <a href="./pages/login.php"><img src="./assets/imeges/lupa.png" alt="#" class="avt"></a>
+            </div>
+        </header>
+
+    </body>
+</html>
---
+++ pages/login.php
@@ -0,0 +1,13 @@
+<head>
+    <title>Авт</title>
+    <link rel="stylesheet" href="../assets/style.css">
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text"placeholder="пароль" name="password">
+        <input type="text"placeholder="логин" name="login">
+    </form>
+</body>
+<?php
+
+?>
